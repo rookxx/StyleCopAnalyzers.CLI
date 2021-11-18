72722a2d89aaaa39fbfa60f6133430741bc8799f
rookxx <vre1018@gmail.com>
2021-11-18 11:54:46 +0900

    Update StylecopAnalyzers version

--- src/Sandbox/FixerTestClass1.cs
+++ src/Sandbox/FixerTestClass1.cs
@@ -3,15 +3,18 @@ using System;
 namespace Sandbox
 {
     using System.Collections;
-    public class FixerTestClass01 {
+    public class FixerTestClass01
+    {
         public int _member1;
+        public List<int> _list = new();
 
-        public void FixerTestMethod() {
-            Console.WriteLine(_member1) ;
+        public void FixerTestMethod()
+        {
+            Console.WriteLine(_member1);
 
             var x = 1;
-            var y=0;
-            Console.WriteLine(x+y);
+            var y = 0;
+            Console.WriteLine(x + y);
         }
     }
 }
